16a3fc157737b0bc5bde024e01dc2c959a47face
sammusama <sam.kahrom@myy.haaga-helia.fi>
2021-04-07 13:52:33 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,101 @@
+# To solve your task you might (or might not) need to import additional libraries
+from flask import Flask, render_template, redirect, url_for, request, logging
+from dateutil.parser import parse
+from datetime import datetime
+import requests
+import json
+
+app = Flask(__name__, static_url_path='/static')
+
+# Headers for REST API call. 
+# Paste the API-key you have been provided as the value for "x-api-key"
+headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/hal+json",
+        "x-api-key": "860393E332148661C34F8579297ACB000E15F770AC4BD945D5FD745867F590061CAE9599A99075210572"
+        }
+
+
+# Example of function for REST API call to get data from Lime
+def get_api_data(headers, url):
+    # First call to get first data page from the API
+    response = requests.get(url=url,
+                            headers=headers,
+                            data=None,
+                            verify=False)
+
+    # Convert the response string into json data and get embedded limeobjects
+    json_data = json.loads(response.text)
+    limeobjects = json_data.get("_embedded").get("limeobjects")
+
+    # Check for more data pages and get thoose too
+    nextpage = json_data.get("_links").get("next")
+    while nextpage is not None:
+        url = nextpage["href"]
+        response = requests.get(url=url,
+                                headers=headers,
+                                data=None,
+                                verify=False)
+
+        json_data = json.loads(response.text)
+        limeobjects += json_data.get("_embedded").get("limeobjects")
+        nextpage = json_data.get("_links").get("next")
+
+    return limeobjects
+
+
+# Index page
+@app.route('/')
+def index():
+
+    return render_template('home.html')
+
+
+# Example page
+@app.route('/example')
+def example():
+
+    # Example of API call to get deals
+    base_url = "https://api-test.lime-crm.com/api-test/api/v1/limeobject/deal/"
+    params = "?_limit=50"
+    url = base_url + params
+    response_deals = get_api_data(headers=headers, url=url)
+
+    """
+    [YOUR CODE HERE]
+    In this exmaple, this is where you can do something with the data in
+    'response_deals' before you return it below.
+    """
+
+    if len(response_deals) > 0:
+        return render_template('example.html', deals=response_deals)
+    else:
+        msg = 'No deals found'
+        return render_template('example.html', msg=msg)
+
+
+# You can add more pages to your app, like this:
+@app.route('/myroute')
+def myroute():
+    mydata = [{'name': 'apple'}, {'name': 'mango'}, {'name': 'banana'}]
+
+    return render_template('mytemplate.html', items=mydata)
+
+
+"""
+You also have to create the mytemplate.html page inside the 'templates'
+-folder to be rendered. And then add a link to your page in the 
+_navbar.html-file, located in templates/includes/
+"""
+
+# DEBUGGING
+"""
+If you want to debug your app, one of the ways you can do that is to use:
+import pdb; pdb.set_trace()
+Add that line of code anywhere, and it will act as a breakpoint and halt
+your application
+"""
+
+if __name__ == '__main__':
+    app.secret_key = 'somethingsecret'
+    app.run(debug=True)
